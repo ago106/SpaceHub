37122fe28c746ada44132ad8aef895f093ab8c84
ago106 <168357108+ago106@users.noreply.github.com>
2025-06-25 22:32:35 +0700

    Update IDS.lua

--- LuaCore/Selector/IDS.lua
+++ LuaCore/Selector/IDS.lua
@@ -1,5 +1,19 @@
 local IDS = {
-    [1] = "https://youtube.com"
+    [537413528] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BABFT.lua", -- BABFT
+    [85896571713843] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BGSI.lua", -- Bubble gum simulator infinity
+    [109652885385286] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Backyard.lua", -- Dig The backyard
+    [78041891124723] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BloodDebt.lua", -- Blood Debt
+    [101949297449238] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BuildIsland.lua", -- Build an Island
+    [70876832253163] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/DeadRailsBond.lua", -- Dead Rails bond farm
+    [87700573492940] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Drill.lua", -- Untilted drill game
+    [126884695634066] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Garden.lua", -- Grow a Garden
+    [138878871969281] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Skinwalkers.lua", -- lobby/game
+    [94725155944518] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Skinwalkers.lua", -- lobby/game
+    [1537690962] = "", -- BSS
+    [142823291] = "", -- MM2
+    [16732694052] = "https://gist.githubusercontent.com/ago106/7c61de715186c68b7e0580b4e7b1f8a0/raw/", -- Fisch sea 1 | should work
+    [18901165922] = "https://gist.githubusercontent.com/ago106/5a42b815cccc68081f568a898ef55aab/raw", -- Pets Go | No key
+    --[] = "",
 }
 
 return IDS
