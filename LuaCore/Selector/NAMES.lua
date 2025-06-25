3901f07e3b302f4fbfb61631b23ecb64cb0162d5
ago106 <168357108+ago106@users.noreply.github.com>
2025-06-25 23:59:34 +0700

    Update NAMES.lua

--- LuaCore/Selector/NAMES.lua
+++ LuaCore/Selector/NAMES.lua
@@ -1,5 +1,18 @@
 local Names = {
-    ["BABFT"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BABFT.lua"
+    ["BABFT"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BABFT.lua", -- BABFT
+    ["BGSI"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BGSI.lua", -- Bubble gum simulator infinity
+    ["Backyard"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Backyard.lua", -- Dig The backyard
+    ["BloodDebt"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BloodDebt.lua", -- Blood Debt
+    ["BuildIsland"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/BuildIsland.lua", -- Build an Island
+    ["DeadRailsBond"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/DeadRailsBond.lua", -- Dead Rails bond farm
+    ["Drill"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Drill.lua", -- Untilted drill game
+    ["Garden"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Garden.lua", -- Grow a Garden
+    ["Skinwalkers"] = "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Scripts/Skinwalkers.lua", -- lobby/game
+    ["BSS"] = "", -- BSS
+    ["MM2"] = "", -- MM2
+    ["none"] = "https://gist.githubusercontent.com/ago106/7c61de715186c68b7e0580b4e7b1f8a0/raw/", -- Fisch sea 1 | should work
+    ["none"] = "https://gist.githubusercontent.com/ago106/5a42b815cccc68081f568a898ef55aab/raw", -- Pets Go | No key
+    --[] = "",
 }
 
 return Names
