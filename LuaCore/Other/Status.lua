c696903873dabc296b21d9ef4afcd75992324d1c
ago106 <168357108+ago106@users.noreply.github.com>
2025-10-01 22:59:37 +0700

    Update Status.lua

--- LuaCore/Other/Status.lua
+++ LuaCore/Other/Status.lua
@@ -1,7 +1,7 @@
-local Status = "Active" --Active/Maintenance
+local Status = "Maintenance" --Active/Maintenance
 print("[Space Hub]: " .. Status)
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 if Status == "Maintenance" then
-  LocalPlayer:Kick("Script on Maintenance. | " .. getgemv().DiscordFull)
+  LocalPlayer:Kick("Script on Maintenance. | " .. getgenv().DiscordFull)
 end
