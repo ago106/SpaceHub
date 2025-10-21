673289c17e20cf7d07083e12bbd297f139dc559b
ago106 <user@users.noreply.github.com>
2025-10-21 23:05:26 +0700

    0247700b-62de-4bf4-80e4-a26f709f6d1b

--- Protection.lua
+++ Protection.lua
@@ -4,11 +4,11 @@ local Protection = {
     Date = 21,
     Hour = 23,
     Minute = 5,
-    Second = 18,
-    LuaCore = "1569fb7f-84b9-498e-9974-873af350279d",
-    Premium = "19717118090088453978787990039802",
-    KeySystem = "4520974927950767949607390990869070506474975449260856210035643152",
-    Validation = "n8kkt01q9c8yipjtskbx9vjg43mqu7iofa4uvzs94jq0g6g4ye0r0eiq5qh10tvqmhxinb7npgkymvbdgmbooh4lqwh4q7w9wfs69acfbo2jgulu369jo3f690obi2d9"
+    Second = 26,
+    LuaCore = "0247700b-62de-4bf4-80e4-a26f709f6d1b",
+    Premium = "35380931934891921459289298127689",
+    KeySystem = "0408151975233573220290572414770590985277181325741039573621308739",
+    Validation = "76s03oxpdt4u2hu584rnhbck67o89q6e8wok4gqds37mku9r6m4phv99p0awcmijyffc90yu1dqc3zbkao3j5h9pkfebb7b439v7idjkjn8ds8o435j1y7k2xg8022aw"
 }
 
 return Protection
